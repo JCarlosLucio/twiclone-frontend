335adea25894a23df667768d31f95f4a4bf37d9e
jcLucio <c.lucio3@gmail.com>
2021-08-25 23:38:44 -0500

    Add currentUser query - Add provisional logout button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useQueryClient } from 'react-query';
 import { Feed } from './Feed';
+import { Auth } from './Auth';
+import storage from './utils/storage';
+import { useQuery } from 'react-query';
+import { getCurrentUser } from './services/user';
 
 const App = () => {
+  const queryClient = useQueryClient();
+  const {
+    data: user,
+    error,
+    isLoading,
+    isError,
+  } = useQuery('currentUser', getCurrentUser, { initialData: null });
+
+  const logout = () => {
+    storage.clearUser();
+    queryClient.resetQueries('currentUser');
+  };
+
+  if (isLoading) return <p>Loading ...</p>;
+
+  if (isError) return <p>Error: {error.message}</p>;
+
   return (
     <div>
       <h1>Twiclone Frontend</h1>
+      <button onClick={logout}>logout</button>
+      {!user && <p>PLEASE LOGIN!</p>}
+      <Auth />
       <Feed />
     </div>
   );
