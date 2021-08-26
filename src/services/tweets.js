890abb3354bbd60ac66ff171eeae3ae4d836a7f8
jcLucio <c.lucio3@gmail.com>
2021-08-25 21:26:24 -0500

    Add createTweet to tweets service

--- src/services/tweets.js
+++ src/services/tweets.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 import { API_URL } from '../constants';
+import { authHeader } from './auth-header';
 
 const baseUrl = `${API_URL}/tweets`;
 
 export const getAllTweets = async () => {
   const response = await axios.get(baseUrl);
+  return response.data;
+};
+
+export const createTweet = async (newTweet) => {
+  const response = await axios.post(baseUrl, newTweet, authHeader());
   return response.data;
 };
