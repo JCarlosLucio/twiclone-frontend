dce228655d5c4baf796caeef73ca724f84518a5f
jcLucio <c.lucio3@gmail.com>
2021-09-11 18:01:33 -0500

    Make Auth form work for Login/Register - Refactor Login form to Login component - Export Login/Register components (src/Auth/index.js) - Add Register form to Auth component - Add toggle for Login/Register

---
+++ src/Auth/Login.jsx
@@ -0,0 +1,46 @@
+import { useForm } from 'react-hook-form';
+import { useMutation, useQueryClient } from 'react-query';
+import { login } from '../services/auth';
+
+export const Login = () => {
+  const queryClient = useQueryClient();
+  const { register, handleSubmit, formState: { errors } } = useForm(); // prettier-ignore
+  const { mutateAsync, isLoading } = useMutation(login, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries('currentUser');
+      console.log(data);
+      // TODO: Navigate to Homepage
+    },
+  });
+
+  const onSubmit = async (formData) => {
+    console.log(formData);
+    await mutateAsync({ ...formData });
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          placeholder="email"
+          {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email?.type === 'required' && 'Email is required'}
+        {errors.email?.type === 'pattern' && 'Email must be a valid email'}
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="password"
+          {...register('password', { required: true, maxLength: 128 })}
+        />
+        {errors.password?.type === 'required' && 'Password is required'}
+
+        <button type="submit">{isLoading ? 'Logging in...' : 'Login'}</button>
+      </form>
+    </div>
+  );
+};
