fe8c156383c17863c1f71738027e939d528d3796
jcLucio <c.lucio3@gmail.com>
2021-09-18 23:17:51 -0500

    Add InfiniteScroll to TweetList component - Change useQuery to useInfiniteQuery - Change getAllTweets to getTweets - Add InfiniteScroll component

--- src/shared/TweetList.jsx
+++ src/shared/TweetList.jsx
@@ -1,19 +1,36 @@
-import { useQuery } from 'react-query';
+import { useInfiniteQuery } from 'react-query';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { Tweet } from './Tweet';
-import { getAllTweets } from '../services/tweets';
+import { getTweets } from '../services/tweets';
 
 export const TweetList = () => {
-  const { data, error, isLoading, isError } = useQuery('tweets', getAllTweets);
+  const { data, fetchNextPage, hasNextPage, isLoading, isError, error } =
+    useInfiniteQuery('tweets', ({ pageParam = 1 }) => getTweets(pageParam), {
+      getNextPageParam: (page) =>
+        page.currentPage >= page.lastPage ? undefined : page.currentPage + 1,
+    });
 
   if (isLoading) return <p>Loading ...</p>;
 
   if (isError) return <p>Error: {error.message}</p>;
 
   return (
-    <div>
-      {data.map((tweet) => (
-        <Tweet key={tweet.id} tweet={tweet} />
-      ))}
-    </div>
+    <InfiniteScroll
+      dataLength={data.pages.length} //This is important field to render the next data
+      next={fetchNextPage}
+      hasMore={hasNextPage}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: 'center' }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+    >
+      {data.pages.map((pageData) => {
+        return pageData.tweets.map((tweet) => {
+          return <Tweet key={tweet.id} tweet={tweet} />;
+        });
+      })}
+    </InfiniteScroll>
   );
 };
