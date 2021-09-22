84d07c6a11156afa0cbd5d1318cf89a3bf56283f
jcLucio <c.lucio3@gmail.com>
2021-09-21 19:15:39 -0500

    Add image to Tweet component

--- src/shared/Tweet.jsx
+++ src/shared/Tweet.jsx
@@ -10,6 +10,11 @@ export const Tweet = ({ tweet }) => {
           <span>{`@${tweet.user.username}`}</span>
         </h3>
       </Link>
+      <img
+        style={{ width: '504px' }}
+        src={tweet.images[0].url}
+        alt="tweet image"
+      />
       <p>{tweet.content}</p>
     </div>
   );
