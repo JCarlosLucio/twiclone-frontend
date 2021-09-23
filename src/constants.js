d255d69b35c79f095aa25947a1691b743a54b721
jcLucio <c.lucio3@gmail.com>
2021-09-23 16:46:17 -0500

    Add queryKeys - Add tweets/currentUser keys to queryKeys

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
 export const API_URL = process.env.REACT_APP_API_SERVER;
 export const USER_STORAGE_KEY = 'loggedTwicloneUser';
+
+export const queryKeys = {
+  tweets: 'tweets',
+  currentUser: 'currentUser',
+};
