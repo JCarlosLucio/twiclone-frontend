7b7b9471dedb7512e0004692caaff7385585af6c
jcLucio <c.lucio3@gmail.com>
2021-09-23 23:47:53 -0500

    Update TweetForm to also work for Tweet replies

--- src/shared/TweetForm.jsx
+++ src/shared/TweetForm.jsx
@@ -3,7 +3,7 @@ import { useMutation, useQueryClient } from 'react-query';
 import { createTweet } from '../services/tweets';
 import { queryKeys } from '../constants';
 
-export const TweetForm = () => {
+export const TweetForm = ({ tweet }) => {
   const queryClient = useQueryClient();
   const { register, handleSubmit, formState: { errors } } = useForm(); // prettier-ignore
   const { mutateAsync, isLoading } = useMutation(createTweet, {
@@ -25,7 +25,9 @@ export const TweetForm = () => {
 
   const onSubmit = async (data, e) => {
     const formData = new FormData();
-
+    if (tweet?.id) {
+      formData.append('parent', tweet.id);
+    }
     formData.append('content', data.content);
 
     [...data.images].forEach((image) => {
@@ -37,39 +39,54 @@ export const TweetForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="content">Content</label>
-      <input
-        type="text"
-        placeholder="content"
-        {...register('content', { required: true, maxLength: 280 })}
-      />
-      <p style={{ color: 'red' }}>
-        {errors.content?.type === 'required' && 'Content is required'}
-      </p>
+    <div>
+      {tweet && (
+        <div>
+          <h3>{tweet.user.name}</h3>
+          <p>{tweet.content}</p>
+        </div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="content">Content</label>
+        <input
+          type="text"
+          placeholder="content"
+          {...register('content', { required: true, maxLength: 280 })}
+        />
+        <p style={{ color: 'red' }}>
+          {errors.content?.type === 'required' && 'Content is required'}
+        </p>
 
-      <input
-        {...register('images', {
-          validate: {
-            maxFiles: (files) => files.length <= 4 || 'Max 4 images',
-            maxSize: (files) =>
-              [...files].every((file) => file?.size < 3 * 1024 * 1024) ||
-              'Max 3MB',
-            acceptedFormats: (files) =>
-              [...files].every((file) =>
-                ['image/png', 'image/jpeg', 'image/jpg', 'image/gif'].includes(
-                  file?.type
-                )
-              ) || 'Only PNG, JPG, JPEG e GIF',
-          },
-        })}
-        type="file"
-        name="images"
-        multiple
-      />
-      <p style={{ color: 'red' }}>{errors?.images && errors?.images.message}</p>
+        <input
+          {...register('images', {
+            validate: {
+              maxFiles: (files) => files.length <= 4 || 'Max 4 images',
+              maxSize: (files) =>
+                [...files].every((file) => file?.size < 3 * 1024 * 1024) ||
+                'Max 3MB',
+              acceptedFormats: (files) =>
+                [...files].every((file) =>
+                  [
+                    'image/png',
+                    'image/jpeg',
+                    'image/jpg',
+                    'image/gif',
+                  ].includes(file?.type)
+                ) || 'Only PNG, JPG, JPEG e GIF',
+            },
+          })}
+          type="file"
+          name="images"
+          multiple
+        />
+        <p style={{ color: 'red' }}>
+          {errors?.images && errors?.images.message}
+        </p>
 
-      <button type="submit">{isLoading ? 'Tweeting...' : 'Tweet'}</button>
-    </form>
+        <button type="submit">
+          {isLoading ? 'Tweeting...' : tweet ? 'Reply' : 'Tweet'}
+        </button>
+      </form>
+    </div>
   );
 };
