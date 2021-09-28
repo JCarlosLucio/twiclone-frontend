6cb08c5e238144adb06bab6509602eaee59bd414
jcLucio <c.lucio3@gmail.com>
2021-09-28 13:37:42 -0500

    Add followUser to user service

--- src/services/user.js
+++ src/services/user.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { authHeader } from './auth-header';
 import { API_URL } from '../constants';
 
 const baseUrl = `${API_URL}/users`;
@@ -6,6 +7,18 @@ const baseUrl = `${API_URL}/users`;
 export const getUser = async (username) => {
   if (username) {
     const response = await axios.get(`${baseUrl}/${username}`);
+    return response.data;
+  }
+  return null;
+};
+
+export const followUser = async (userId) => {
+  if (userId) {
+    const response = await axios.post(
+      `${baseUrl}/${userId}/follow`,
+      null,
+      authHeader()
+    );
     return response.data;
   }
   return null;
