222c260a534004806e091463e983063a47c94ab7
jcLucio <c.lucio3@gmail.com>
2021-09-29 10:40:47 -0500

    Add useMe hook - Add updateUser - Add clearUser

---
+++ src/shared/hooks/useMe.js
@@ -0,0 +1,29 @@
+import { useQuery, useQueryClient } from 'react-query';
+import { queryKeys } from '../../constants';
+import { getMe } from '../../services/auth';
+import storage from '../../utils/storage';
+
+export const useMe = () => {
+  const queryClient = useQueryClient();
+  const { data: me = storage.loadUser() } = useQuery(queryKeys.me, getMe, {
+    enabled: !!storage.loadUser(),
+  });
+
+  const updateUser = (newUser) => {
+    // update user(me) in localStorage
+    storage.saveUser(newUser);
+
+    // populate user profile in ReactQuery client
+    queryClient.setQueryData(queryKeys.me, newUser);
+  };
+
+  const clearUser = () => {
+    // update user(me) in localStorage
+    storage.clearUser();
+
+    // reset user to null in ReactQuery client
+    queryClient.setQueryData(queryKeys.me, null);
+  };
+
+  return { me, updateUser, clearUser };
+};
