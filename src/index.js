9dea99dbcabb734f6323fc1a900e6139e2f1e95e
jcLucio <c.lucio3@gmail.com>
2021-09-30 22:21:43 -0500

    Add SnackbarProvider with SnackbarUtilsConfigurator - SnackbarProvider gives access to Snackbars to components - SnackbarUtils Configurator gives access to Snackbars from outside components

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { SnackbarProvider } from 'notistack';
 import { queryClient } from './utils/query-client';
+import { SnackbarUtilsConfigurator } from './utils/SnackbarUtils';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <App />
-      </Router>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <SnackbarProvider>
+      <SnackbarUtilsConfigurator />
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <App />
+        </Router>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </SnackbarProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
