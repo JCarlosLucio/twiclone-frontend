f9aee27306e243eb2f576c8a56fd4434e3c87725
jcLucio <c.lucio3@gmail.com>
2021-09-30 22:23:12 -0500

    Add queryErrorHandler to queryClient - Add queryErrorHanlder function to show a Snackbar on Error

--- src/utils/query-client.js
+++ src/utils/query-client.js
@@ -1,11 +1,24 @@
 import { QueryClient } from 'react-query';
+import SnackbarUtils from './SnackbarUtils';
+
+const queryErrorHandler = (error) => {
+  const errorMessage =
+    error?.response?.data?.error ||
+    // remove the initial 'Error: ' that accompanies many errors
+    error.toString().replace(/^Error:\s*/, '') ||
+    'error connecting to server';
+
+  SnackbarUtils.error(errorMessage);
+};
 
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      onError: queryErrorHandler,
       refetchOnWindowFocus: false,
       refetchOnMount: false,
       refetchOnReconnect: false,
+      retry: 1,
     },
   },
 });
