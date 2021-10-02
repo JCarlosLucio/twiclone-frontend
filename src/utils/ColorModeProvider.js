eb84505bff999e4db6efc557bead0fb4fefae799
jcLucio <c.lucio3@gmail.com>
2021-10-02 18:45:37 -0500

    Add ColorModeProvider for handling theming

---
+++ src/utils/ColorModeProvider.js
@@ -0,0 +1,39 @@
+import { createContext, useMemo, useState } from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { getDesignTokens } from './themes';
+
+export const ColorModeContext = createContext({
+  mode: '',
+  toggleColorMode: () => {},
+});
+
+const ColorModeProvider = ({ children }) => {
+  // State to hold the selected theme name
+  const [mode, setMode] = useState('dark');
+
+  const colorMode = useMemo(
+    () => ({
+      mode,
+      // The dark mode switch would invoke this method
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    []
+  );
+
+  // Update the theme only if the mode changes
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export default ColorModeProvider;
