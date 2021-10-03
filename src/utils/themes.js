53a9ef5bb074dc31003c5277873f8ed238d86b0b
jcLucio <c.lucio3@gmail.com>
2021-10-02 22:48:15 -0500

    Update themes' secondary colors

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -4,16 +4,22 @@ import grey from '@mui/material/colors/grey';
 const lightTheme = {
   // palette values for light mode
   primary: blue,
+  secondary: {
+    main: grey[900],
+    light: grey[500],
+    dark: grey[900],
+  },
   divider: blue[200],
   text: {
     primary: grey[900],
-    secondary: grey[800],
+    secondary: grey[900],
   },
 };
 
 const darkTheme = {
   // palette values for dark mode
   primary: blue,
+  secondary: grey,
   divider: blue[700],
   background: {
     default: '#000',
