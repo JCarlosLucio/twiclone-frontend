19fe33949575d358310548eebdc5b2eec447bec2
jcLucio <c.lucio3@gmail.com>
2021-10-03 23:28:57 -0500

    Rename themes.js to theme.js - Update path to theme.js in ColorModeProvider

--- src/utils/ColorModeProvider.js
+++ src/utils/ColorModeProvider.js
@@ -1,7 +1,7 @@
 import { createContext, useMemo, useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { getDesignTokens } from './themes';
+import { getDesignTokens } from './theme';
 
 export const ColorModeContext = createContext({
   mode: '',
