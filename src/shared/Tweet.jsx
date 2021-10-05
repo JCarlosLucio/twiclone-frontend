d7a47c7e5751f8299b72225a6e7908d3459e13d2
jcLucio <c.lucio3@gmail.com>
2021-10-05 01:51:00 -0500

    Reorganize Tweet component with Stack

--- src/shared/Tweet.jsx
+++ src/shared/Tweet.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Stack from '@mui/material/Stack';
 import PropTypes from 'prop-types';
 import { TweetForm } from '../shared';
 import { useLikeTweet } from '../shared/hooks/useLikeTweet';
@@ -21,35 +22,47 @@ export const Tweet = ({ tweet }) => {
   };
 
   return (
-    <div style={{ border: '1px solid blue' }}>
-      <div>
-        <img
-          style={{ width: 40 }}
-          src={tweet.user.avatar.url}
-          alt={`${tweet.user.name} avatar`}
-        />
-        <Link to={`/${tweet.user.username}`}>
-          {tweet.user.name}
+    <Stack
+      direction="row"
+      alignItems="flex-start"
+      spacing={2}
+      sx={{ borderBottom: '1px solid', borderColor: 'divider', p: 2 }}
+    >
+      <img
+        style={{ width: 40 }}
+        src={tweet.user.avatar.url}
+        alt={`${tweet.user.name} avatar`}
+      />
+
+      <Stack spacing={2} sx={{ width: '100%' }}>
+        <Stack direction="row" alignItems="flex-start" spacing={1}>
+          <Link to={`/${tweet.user.username}`}>{tweet.user.name}</Link>
           <span>{`@${tweet.user.username}`}</span>
-        </Link>
-      </div>
-      <p>{tweet.content}</p>
-      {tweet.images && tweet.images.length > 0 && (
-        <img
-          style={{ width: '504px' }}
-          src={tweet.images[0].url}
-          alt="tweet image"
-        />
-      )}
-      <div>
-        <button onClick={toggleTweetForm}>Reply</button>
-        <button onClick={handleLike} disabled={isLoading}>
-          {isLoading ? 'liking...' : isLiked ? 'unlike' : 'like'}
-        </button>
-        {likesCount}
-      </div>
-      {showTweetForm && <TweetForm tweet={tweet} />}
-    </div>
+        </Stack>
+
+        <p>{tweet.content}</p>
+
+        {tweet.images && tweet.images.length > 0 && (
+          <img
+            style={{ width: '100%' }}
+            src={tweet.images[0].url}
+            alt="tweet image"
+          />
+        )}
+        <Stack
+          direction="row"
+          justifyContent="space-around"
+          alignItems="center"
+        >
+          <button onClick={toggleTweetForm}>Reply</button>
+          <button onClick={handleLike} disabled={isLoading}>
+            {isLoading ? 'liking...' : isLiked ? 'unlike' : 'like'}
+          </button>
+          {likesCount}
+        </Stack>
+        {showTweetForm && <TweetForm tweet={tweet} />}
+      </Stack>
+    </Stack>
   );
 };
 
