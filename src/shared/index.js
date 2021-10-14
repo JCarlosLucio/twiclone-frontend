68321e98dd72fe391d5029a173b2030f0993dce7
jcLucio <c.lucio3@gmail.com>
2021-10-13 21:42:47 -0500

    Re-export CharCounter, ImagePreviews and CustomModal components

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,3 +1,6 @@
+export * from './CharCounter';
+export * from './ImagePreviews';
 export * from './Tweet';
 export * from './TweetList';
 export * from './TweetForm';
+export * from './CustomModal';
