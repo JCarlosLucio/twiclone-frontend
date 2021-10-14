9bbee860f550498bab95266fd07f3aff3d6cf152
jcLucio <c.lucio3@gmail.com>
2021-10-14 02:02:15 -0500

    Refactor user header with UserHeader component

--- src/shared/Tweet.jsx
+++ src/shared/Tweet.jsx
@@ -1,6 +1,5 @@
 import { BsChat, BsHeart, BsHeartFill, BsUpload } from 'react-icons/bs';
 import { AiOutlineRetweet } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import ImageList from '@mui/material/ImageList';
@@ -8,11 +7,11 @@ import ImageListItem from '@mui/material/ImageListItem';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
+import { UserHeader } from './UserHeader';
 import { CustomModal, TweetForm } from '../shared';
 import { useLikeTweet } from '../shared/hooks/useLikeTweet';
 import { useModal } from '../shared/hooks/useModal';
 import { prepareForImageList } from '../utils/images';
-import { dateFromNow } from '../utils/date';
 
 export const Tweet = ({ tweet }) => {
   const { open, handleOpen, handleClose } = useModal(false);
@@ -38,37 +37,7 @@ export const Tweet = ({ tweet }) => {
       <Avatar src={tweet.user.avatar.url} alt={`${tweet.user.name}`} />
 
       <Stack sx={{ width: '100%' }}>
-        <Stack
-          direction="row"
-          alignItems="flex-start"
-          spacing={0.5}
-          sx={{
-            '& a': {
-              textDecoration: 'none',
-              fontWeight: 700,
-            },
-            '&:hover': {
-              '& a': {
-                textDecoration: 'underline',
-              },
-            },
-          }}
-        >
-          <Typography
-            color="text.primary"
-            to={`/${tweet.user.username}`}
-            component={Link}
-          >
-            {tweet.user.name}
-          </Typography>
-          <Typography color="text.secondary">
-            {`@${tweet.user.username}`}
-          </Typography>
-          <Typography color="text.secondary">·</Typography>
-          <Typography color="text.secondary">
-            {dateFromNow(tweet.createdAt)}
-          </Typography>
-        </Stack>
+        <UserHeader user={tweet.user} createdAt={tweet.createdAt} withLink />
 
         <Stack spacing={1.5}>
           <Typography>{tweet.content}</Typography>
