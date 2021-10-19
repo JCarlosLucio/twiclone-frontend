5bb434a36ac04c6316e5e864d40338bf159c94fe
jcLucio <c.lucio3@gmail.com>
2021-10-18 23:24:38 -0500

    Add BannerPreview to EditProfileForm component - Style avatar to overlap over banner - Organize Stacks so padding/spacing was applied correctly

--- src/Profile/EditProfileForm.jsx
+++ src/Profile/EditProfileForm.jsx
@@ -9,6 +9,7 @@ import PropTypes from 'prop-types';
 import { AvatarPreview } from './AvatarPreview';
 import { useUpdateMe } from './hooks/useUpdateMe';
 import SnackbarUtils from '../utils/SnackbarUtils';
+import { BannerPreview } from './BannerPreview';
 
 export const EditProfileForm = ({ me, handleClose }) => {
   const [avatar, setAvatar] = useState();
@@ -40,73 +41,26 @@ export const EditProfileForm = ({ me, handleClose }) => {
   };
 
   return (
-    <Stack
-      component="form"
-      onSubmit={handleSubmit(onSubmit)}
-      alignItems="flex-start"
-      spacing={2}
-      m={2}
-    >
-      <label htmlFor="banner-file-input">
-        <input
-          id="banner-file-input"
-          type="file"
-          name="banner"
-          accept="image/jpeg,image/png,image/gif"
-          style={{ display: 'none' }}
-          {...register('banner', {
-            onChange: ({ target }) => {
-              const [file] = [...target.files];
-              // resets value of banner
-              setValue('banner', undefined);
-
-              const maxSize = file.size > 3 * 1024 * 1024;
-
-              const acceptedFormats = ![
-                'image/png',
-                'image/jpeg',
-                'image/jpg',
-                'image/gif',
-              ].includes(file?.type);
-
-              if (maxSize) {
-                SnackbarUtils.error('Please choose photos up to 3MB.');
-              }
-              if (acceptedFormats) {
-                SnackbarUtils.error(
-                  'Please choose PNG, JPG, JPEG or GIF photos.'
-                );
-              }
-              if (maxSize || acceptedFormats) return;
-
-              setBanner(file);
-            },
-          })}
-        />
-        <IconButton color="secondary" aria-label="Add banner" component="span">
-          <MdOutlineCameraEnhance />
-        </IconButton>
-      </label>
-
+    <Stack component="form" onSubmit={handleSubmit(onSubmit)}>
       <Stack
         alignItems="center"
         justifyContent="center"
         sx={{ position: 'relative' }}
       >
-        <AvatarPreview defaultAvatar={me?.avatar?.url} avatar={avatar} />
+        <BannerPreview defaultBanner={me?.banner?.url} banner={banner} />
 
-        <label htmlFor="avatar-file-input" style={{ position: 'absolute' }}>
+        <label htmlFor="banner-file-input" style={{ position: 'absolute' }}>
           <input
-            id="avatar-file-input"
+            id="banner-file-input"
             type="file"
-            name="avatar"
+            name="banner"
             accept="image/jpeg,image/png,image/gif"
             style={{ display: 'none' }}
-            {...register('avatar', {
+            {...register('banner', {
               onChange: ({ target }) => {
                 const [file] = [...target.files];
-                // resets value of avatar
-                setValue('avatar', undefined);
+                // resets value of banner
+                setValue('banner', undefined);
 
                 const maxSize = file.size > 3 * 1024 * 1024;
 
@@ -127,14 +81,13 @@ export const EditProfileForm = ({ me, handleClose }) => {
                 }
                 if (maxSize || acceptedFormats) return;
 
-                setAvatar(file);
+                setBanner(file);
               },
             })}
           />
-
           <IconButton
             color="secondary"
-            aria-label="Add avatar"
+            aria-label="Add banner"
             component="span"
           >
             <MdOutlineCameraEnhance />
@@ -142,55 +95,112 @@ export const EditProfileForm = ({ me, handleClose }) => {
         </label>
       </Stack>
 
-      <TextField
-        type="text"
-        variant="outlined"
-        size="large"
-        fullWidth
-        label="Name"
-        placeholder="Name"
-        error={!!errors.name}
-        helperText={
-          (errors.name?.type === 'required' && 'Name is required') ||
-          (errors.name?.type === 'maxLength' &&
-            'Name must be 280 characters or less')
-        }
-        {...register('name', { required: true, maxLength: 280 })}
-      />
+      <Stack p={2}>
+        <Stack alignItems="flex-start" sx={{ mt: '-80px' }}>
+          <Stack
+            alignItems="center"
+            justifyContent="center"
+            sx={{ position: 'relative' }}
+          >
+            <AvatarPreview defaultAvatar={me?.avatar?.url} avatar={avatar} />
 
-      <TextField
-        type="text"
-        variant="outlined"
-        size="large"
-        fullWidth
-        label="Bio"
-        placeholder="Bio"
-        error={!!errors.bio}
-        helperText={
-          errors.bio?.type === 'maxLength' &&
-          'Bio must be 160 characters or less'
-        }
-        {...register('bio', { maxLength: 160 })}
-      />
+            <label htmlFor="avatar-file-input" style={{ position: 'absolute' }}>
+              <input
+                id="avatar-file-input"
+                type="file"
+                name="avatar"
+                accept="image/jpeg,image/png,image/gif"
+                style={{ display: 'none' }}
+                {...register('avatar', {
+                  onChange: ({ target }) => {
+                    const [file] = [...target.files];
+                    // resets value of avatar
+                    setValue('avatar', undefined);
 
-      <TextField
-        type="text"
-        variant="outlined"
-        fullWidth
-        size="large"
-        label="Location"
-        placeholder="Location"
-        error={!!errors.location}
-        helperText={
-          errors.location?.type === 'maxLength' &&
-          'Location must be 30 characters or less'
-        }
-        {...register('location', { maxLength: 30 })}
-      />
+                    const maxSize = file.size > 3 * 1024 * 1024;
 
-      <Button type="submit" size="large" variant="contained" fullWidth>
-        {isLoading ? 'Saving...' : 'Save'}
-      </Button>
+                    const acceptedFormats = ![
+                      'image/png',
+                      'image/jpeg',
+                      'image/jpg',
+                      'image/gif',
+                    ].includes(file?.type);
+
+                    if (maxSize) {
+                      SnackbarUtils.error('Please choose photos up to 3MB.');
+                    }
+                    if (acceptedFormats) {
+                      SnackbarUtils.error(
+                        'Please choose PNG, JPG, JPEG or GIF photos.'
+                      );
+                    }
+                    if (maxSize || acceptedFormats) return;
+
+                    setAvatar(file);
+                  },
+                })}
+              />
+
+              <IconButton
+                color="secondary"
+                aria-label="Add avatar"
+                component="span"
+              >
+                <MdOutlineCameraEnhance />
+              </IconButton>
+            </label>
+          </Stack>
+        </Stack>
+
+        <Stack spacing={3} mt={2}>
+          <TextField
+            type="text"
+            variant="outlined"
+            size="large"
+            label="Name"
+            placeholder="Name"
+            error={!!errors.name}
+            helperText={
+              (errors.name?.type === 'required' && 'Name is required') ||
+              (errors.name?.type === 'maxLength' &&
+                'Name must be 280 characters or less')
+            }
+            {...register('name', { required: true, maxLength: 280 })}
+          />
+
+          <TextField
+            type="text"
+            variant="outlined"
+            size="large"
+            label="Bio"
+            placeholder="Bio"
+            error={!!errors.bio}
+            helperText={
+              errors.bio?.type === 'maxLength' &&
+              'Bio must be 160 characters or less'
+            }
+            {...register('bio', { maxLength: 160 })}
+          />
+
+          <TextField
+            type="text"
+            variant="outlined"
+            size="large"
+            label="Location"
+            placeholder="Location"
+            error={!!errors.location}
+            helperText={
+              errors.location?.type === 'maxLength' &&
+              'Location must be 30 characters or less'
+            }
+            {...register('location', { maxLength: 30 })}
+          />
+
+          <Button type="submit" size="large" variant="contained">
+            {isLoading ? 'Saving...' : 'Save'}
+          </Button>
+        </Stack>
+      </Stack>
     </Stack>
   );
 };
