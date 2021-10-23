ad9ec709a94ba4be91df9a2353b700d21719b7a0
jcLucio <c.lucio3@gmail.com>
2021-10-22 23:44:37 -0500

    Update UserHeader to stopPropagation when withLink is true - Add textDecoration for date - Add to/onClick props when withLink is true - Make component a Link when withLink is true

--- src/shared/UserHeader.jsx
+++ src/shared/UserHeader.jsx
@@ -6,38 +6,43 @@ import { dateFromNow } from '../utils/date';
 
 export const UserHeader = ({ user, createdAt, withLink = false }) => {
   return (
-    <Stack
-      direction="row"
-      alignItems="flex-start"
-      spacing={0.5}
-      sx={{
-        '& a': {
+    <Stack direction="row" alignItems="flex-start" spacing={0.5}>
+      <Stack
+        direction="row"
+        alignItems="flex-start"
+        {...(withLink && {
+          to: `/${user.username}`,
+          onClick: (e) => e.stopPropagation(),
+        })}
+        component={withLink ? Link : 'div'}
+        sx={{
           textDecoration: 'none',
-          fontWeight: 700,
-        },
-        '&:hover': {
-          '& a': {
-            textDecoration: 'underline',
+          '&:hover span:first-of-type': {
+            textDecoration: withLink ? 'underline 2px' : 'none',
           },
-        },
-      }}
-    >
-      {withLink ? (
-        <Typography
-          color="text.primary"
-          to={`/${user.username}`}
-          component={Link}
-        >
-          {user.name}
-        </Typography>
-      ) : (
+        }}
+      >
         <Typography color="text.primary" sx={{ fontWeight: 700 }}>
           {user.name}
         </Typography>
-      )}
-      <Typography color="text.secondary">{`@${user.username}`}</Typography>
+        <Typography
+          color="text.secondary"
+          sx={{
+            textDecoration: 'none',
+          }}
+        >{`@${user.username}`}</Typography>
+      </Stack>
       <Typography color="text.secondary">·</Typography>
-      <Typography color="text.secondary">{dateFromNow(createdAt)}</Typography>
+      <Typography
+        color="text.secondary"
+        sx={{
+          '&:hover': {
+            textDecoration: withLink ? 'underline' : 'none',
+          },
+        }}
+      >
+        {dateFromNow(createdAt)}
+      </Typography>
     </Stack>
   );
 };
