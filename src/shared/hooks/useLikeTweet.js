d176478d06b02be2533b9cb49eb3f178902e325e
jcLucio <c.lucio3@gmail.com>
2021-10-22 22:03:25 -0500

    Rename isLoading to isLiking in useLikeTweet hook

--- src/shared/hooks/useLikeTweet.js
+++ src/shared/hooks/useLikeTweet.js
@@ -11,33 +11,36 @@ export const useLikeTweet = (tweetId, likes) => {
 
   const queryClient = useQueryClient();
 
-  const { mutate: like, isLoading } = useMutation(() => likeTweet(tweetId), {
-    onSuccess: (data) => {
-      const tweets = queryClient.getQueryData(queryKeys.tweets);
-      const updatedTweets = {
-        ...tweets,
-        pages: tweets.pages.map((page) => {
-          return {
-            ...page,
-            tweets: page.tweets.map((t) => (t.id === tweetId ? data : t)),
-          };
-        }),
-      };
+  const { mutate: like, isLoading: isLiking } = useMutation(
+    () => likeTweet(tweetId),
+    {
+      onSuccess: (data) => {
+        const tweets = queryClient.getQueryData(queryKeys.tweets);
+        const updatedTweets = {
+          ...tweets,
+          pages: tweets.pages.map((page) => {
+            return {
+              ...page,
+              tweets: page.tweets.map((t) => (t.id === tweetId ? data : t)),
+            };
+          }),
+        };
 
-      // updates tweets query in cache, it better than invalidating queries
-      // because it doesn't move the TweetList with more tweets that were probably
-      // added in the meantime, also saves a call to the server
-      queryClient.setQueryData(queryKeys.tweets, updatedTweets);
+        // updates tweets query in cache, it better than invalidating queries
+        // because it doesn't move the TweetList with more tweets that were probably
+        // added in the meantime, also saves a call to the server
+        queryClient.setQueryData(queryKeys.tweets, updatedTweets);
 
-      // updates tweet state locally so if there were other likes
-      // from the moment the tweet was loaded to when the like button was clicked
-      // the likesCount doesnt look like the action gave more likes than 1
-      setLikesCount((prevVal) => prevVal + (isLiked ? -1 : 1));
-      setIsLiked(!isLiked);
+        // updates tweet state locally so if there were other likes
+        // from the moment the tweet was loaded to when the like button was clicked
+        // the likesCount doesnt look like the action gave more likes than 1
+        setLikesCount((prevVal) => prevVal + (isLiked ? -1 : 1));
+        setIsLiked(!isLiked);
 
-      // TODO: probably needs to be made into an optimistic update
-    },
-  });
+        // TODO: probably needs to be made into an optimistic update
+      },
+    }
+  );
 
-  return { like, isLiked, likesCount, isLoading };
+  return { like, isLiked, likesCount, isLiking };
 };
