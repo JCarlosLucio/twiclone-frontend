dcbbb7d442fc8620da38ae0d3b4b21f1b818f844
jcLucio <c.lucio3@gmail.com>
2021-10-24 03:28:50 -0500

    Update openPopover/closePopover to stopPropagation if e exists

--- src/shared/hooks/usePopover.js
+++ src/shared/hooks/usePopover.js
@@ -4,11 +4,15 @@ export const usePopover = (initialValue = null, id) => {
   const [anchorEl, setAnchorEl] = useState(initialValue);
 
   const openPopover = (e) => {
-    e.stopPropagation();
+    if (e) {
+      e.stopPropagation();
+    }
     setAnchorEl(e.currentTarget);
   };
   const closePopover = (e) => {
-    e.stopPropagation();
+    if (e) {
+      e.stopPropagation();
+    }
     setAnchorEl(null);
   };
 
