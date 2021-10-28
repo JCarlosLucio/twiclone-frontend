616ce396d35e73f1492582ab0ec3b03c1758dc2a
jcLucio <c.lucio3@gmail.com>
2021-10-27 21:11:48 -0500

    Update useInfiniteQuery to accept id for getting replies - Use appropiate queryKey if getting replies

--- src/shared/hooks/useInfiniteTweets.js
+++ src/shared/hooks/useInfiniteTweets.js
@@ -2,11 +2,11 @@ import { useInfiniteQuery } from 'react-query';
 import { queryKeys } from '../../constants';
 import { getTweets } from '../../services/tweets';
 
-export const useInfiniteTweets = () => {
+export const useInfiniteTweets = (id) => {
   const { data, fetchNextPage, hasNextPage, isLoading, isError, error } =
     useInfiniteQuery(
-      queryKeys.tweets,
-      ({ pageParam = 1 }) => getTweets(pageParam, 10),
+      id ? [queryKeys.tweets, id, queryKeys.replies] : queryKeys.tweets,
+      ({ pageParam = 1 }) => getTweets(pageParam, 10, id),
       {
         getNextPageParam: (page) =>
           page.currentPage >= page.lastPage ? undefined : page.currentPage + 1,
