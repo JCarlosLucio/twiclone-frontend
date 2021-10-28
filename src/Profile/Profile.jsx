48822375d1a2483cb52c6dcd02979130616dc4d5
jcLucio <c.lucio3@gmail.com>
2021-10-28 11:51:01 -0500

    Update loading to show TopBar component while loading

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -25,8 +25,6 @@ export const Profile = () => {
     follow(user?.id);
   };
 
-  if (isLoading) return <Loading />;
-
   const isMe = me.id === user?.id;
   const following = me.following.includes(user?.id);
 
@@ -61,124 +59,130 @@ export const Profile = () => {
         </Stack>
       </TopBar>
 
-      <Stack>
-        <Stack>
-          <Stack
-            sx={{
-              height: '100%',
-              backgroundImage: () =>
-                user?.banner?.url ? `url(${user?.banner?.url})` : 'none',
-              backgroundColor: 'backdrop',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-            }}
-          >
-            <Stack sx={{ pb: '190px' }}></Stack>
-          </Stack>
-        </Stack>
-
-        <Stack px={2} pt={1.25}>
-          <Stack
-            direction="row"
-            alignItems="flex-start"
-            justifyContent="space-between"
-          >
-            <Avatar
-              size="large"
-              src={user?.avatar?.url}
-              alt={`${user?.name} avatar`}
-              sx={{
-                mt: '-80px',
-                border: '4px solid',
-                borderColor: 'background.default',
-                backgroundColor: 'divider',
-              }}
-            />
-            {user ? (
-              isMe ? (
-                <Button
-                  variant="secondary"
-                  onClick={openModal}
-                  sx={{ border: '1px solid', borderColor: 'secondary' }}
-                >
-                  Edit profile
-                </Button>
-              ) : (
-                <Button
-                  variant="secondary"
-                  onClick={handleFollow}
-                  sx={{ border: '1px solid', borderColor: 'secondary' }}
-                >
-                  {(isFollowing && 'Following...') ||
-                    (following && 'Following') ||
-                    'Follow'}
-                </Button>
-              )
-            ) : null}
-          </Stack>
-
-          {user ? (
-            <Stack spacing={2}>
-              <Stack>
-                <Typography variant="h6" fontWeight="700">
-                  {user?.name}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                  lineHeight={0.8}
-                >{`@${user?.username}`}</Typography>
-              </Stack>
-
-              <Stack direction="row" alignItems="center" spacing={0.5}>
-                <Stack color="text.secondary">
-                  <BsCalendar3 />
-                </Stack>
-                <Typography variant="body2" color="text.secondary">
-                  {`Joined ${dateMonthYear(user?.createdAt)}`}
-                </Typography>
-              </Stack>
-
-              <Stack direction="row" spacing={3}>
-                <Stack direction="row" alignItems="center" spacing={0.5}>
-                  <Typography fontWeight="700">
-                    {user?.followers?.length}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Followers
-                  </Typography>
-                </Stack>
-                <Stack direction="row" alignItems="center" spacing={0.5}>
-                  <Typography fontWeight="700">
-                    {user?.following?.length}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Following
-                  </Typography>
-                </Stack>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Stack>
+            <Stack>
+              <Stack
+                sx={{
+                  height: '100%',
+                  backgroundImage: () =>
+                    user?.banner?.url ? `url(${user?.banner?.url})` : 'none',
+                  backgroundColor: 'backdrop',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                }}
+              >
+                <Stack sx={{ pb: '190px' }}></Stack>
               </Stack>
             </Stack>
-          ) : (
-            <Typography fontWeight="700">{`@${username}`}</Typography>
-          )}
-        </Stack>
 
-        {!user && (
-          <Stack alignItems="center" my={10}>
-            <Typography variant="h4" fontWeight="700">
-              This account doesn&apos;t exist
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Try searching for another.
-            </Typography>
+            <Stack px={2} pt={1.25}>
+              <Stack
+                direction="row"
+                alignItems="flex-start"
+                justifyContent="space-between"
+              >
+                <Avatar
+                  size="large"
+                  src={user?.avatar?.url}
+                  alt={`${user?.name} avatar`}
+                  sx={{
+                    mt: '-80px',
+                    border: '4px solid',
+                    borderColor: 'background.default',
+                    backgroundColor: 'divider',
+                  }}
+                />
+                {user ? (
+                  isMe ? (
+                    <Button
+                      variant="secondary"
+                      onClick={openModal}
+                      sx={{ border: '1px solid', borderColor: 'secondary' }}
+                    >
+                      Edit profile
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="secondary"
+                      onClick={handleFollow}
+                      sx={{ border: '1px solid', borderColor: 'secondary' }}
+                    >
+                      {(isFollowing && 'Following...') ||
+                        (following && 'Following') ||
+                        'Follow'}
+                    </Button>
+                  )
+                ) : null}
+              </Stack>
+
+              {user ? (
+                <Stack spacing={2}>
+                  <Stack>
+                    <Typography variant="h6" fontWeight="700">
+                      {user?.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      lineHeight={0.8}
+                    >{`@${user?.username}`}</Typography>
+                  </Stack>
+
+                  <Stack direction="row" alignItems="center" spacing={0.5}>
+                    <Stack color="text.secondary">
+                      <BsCalendar3 />
+                    </Stack>
+                    <Typography variant="body2" color="text.secondary">
+                      {`Joined ${dateMonthYear(user?.createdAt)}`}
+                    </Typography>
+                  </Stack>
+
+                  <Stack direction="row" spacing={3}>
+                    <Stack direction="row" alignItems="center" spacing={0.5}>
+                      <Typography fontWeight="700">
+                        {user?.followers?.length}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Followers
+                      </Typography>
+                    </Stack>
+                    <Stack direction="row" alignItems="center" spacing={0.5}>
+                      <Typography fontWeight="700">
+                        {user?.following?.length}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Following
+                      </Typography>
+                    </Stack>
+                  </Stack>
+                </Stack>
+              ) : (
+                <Typography fontWeight="700">{`@${username}`}</Typography>
+              )}
+            </Stack>
+
+            {!user && (
+              <Stack alignItems="center" my={10}>
+                <Typography variant="h4" fontWeight="700">
+                  This account doesn&apos;t exist
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Try searching for another.
+                </Typography>
+              </Stack>
+            )}
+
+            <CustomModal open={open} handleClose={closeModal}>
+              <EditProfileForm me={me} handleClose={closeModal} />
+            </CustomModal>
           </Stack>
-        )}
-
-        <CustomModal open={open} handleClose={closeModal}>
-          <EditProfileForm me={me} handleClose={closeModal} />
-        </CustomModal>
-      </Stack>
+        </>
+      )}
     </Stack>
   );
 };
--- src/TweetDetails/TweetDetails.jsx
+++ src/TweetDetails/TweetDetails.jsx
@@ -28,10 +28,6 @@ export const TweetDetails = () => {
   const { open, openModal, closeModal } = useModal(false);
   const { tweet, isLoading } = useTweetById(tweetId);
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return (
     <Stack
       sx={{
@@ -54,87 +50,97 @@ export const TweetDetails = () => {
           Tweet
         </Typography>
       </TopBar>
-      <Stack px={2} component="article" spacing={2}>
-        <Stack direction="row" spacing={1.5} mt={2}>
-          <Avatar
-            src={tweet.user.avatar.url}
-            alt={`${tweet.user.name}`}
-            component={Link}
-            to={`/${tweet.user.username}`}
-            onClick={(e) => e.stopPropagation()}
-          />
-          <UserHeader user={tweet.user} direction="column" withLink />
-        </Stack>
 
-        <Typography fontSize="1.4rem" lineHeight={1.3}>
-          {tweet.content}
-        </Typography>
-
-        {tweet?.images.length === 1 ? (
-          <Stack my={2} sx={{ borderRadius: '16px', overflow: 'hidden' }}>
-            <img src={tweet.images[0].url} alt="Image" />
-          </Stack>
-        ) : (
-          <TweetImages images={tweet.images} />
-        )}
-
-        <Typography
-          color="text.secondary"
-          sx={{
-            '&:hover': {
-              textDecoration: 'underline',
-              cursor: 'pointer',
-            },
-          }}
-        >
-          {dateFull(tweet.createdAt)}
-        </Typography>
-
-        {tweet?.likes?.length > 0 && (
-          <>
-            <Divider />
-            <Stack direction="row" alignItems="center" spacing={0.5}>
-              <Typography fontWeight="700">{tweet?.likes?.length}</Typography>
-              <Typography variant="body1" color="text.secondary">
-                Likes
-              </Typography>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Stack px={2} component="article" spacing={2}>
+            <Stack direction="row" spacing={1.5} mt={2}>
+              <Avatar
+                src={tweet.user.avatar.url}
+                alt={`${tweet.user.name}`}
+                component={Link}
+                to={`/${tweet.user.username}`}
+                onClick={(e) => e.stopPropagation()}
+              />
+              <UserHeader user={tweet.user} direction="column" withLink />
             </Stack>
-          </>
-        )}
 
-        <Stack spacing={0.5}>
-          <Divider />
-          <Stack direction="row" justifyContent="space-around">
-            <ReplyButton size="medium" handleClick={openModal} />
-            <RetweetButton size="medium" />
-            <LikeButton
-              tweetId={tweet?.id}
-              likes={tweet?.likes}
-              size="medium"
-            />
-            <ShareButton
-              size="medium"
-              tweetId={tweet.id}
-              tweetUsername={tweet.user.username}
+            <Typography fontSize="1.4rem" lineHeight={1.3}>
+              {tweet.content}
+            </Typography>
+
+            {tweet?.images.length === 1 ? (
+              <Stack my={2} sx={{ borderRadius: '16px', overflow: 'hidden' }}>
+                <img src={tweet.images[0].url} alt="Image" />
+              </Stack>
+            ) : (
+              <TweetImages images={tweet.images} />
+            )}
+
+            <Typography
+              color="text.secondary"
+              sx={{
+                '&:hover': {
+                  textDecoration: 'underline',
+                  cursor: 'pointer',
+                },
+              }}
+            >
+              {dateFull(tweet.createdAt)}
+            </Typography>
+
+            {tweet?.likes?.length > 0 && (
+              <>
+                <Divider />
+                <Stack direction="row" alignItems="center" spacing={0.5}>
+                  <Typography fontWeight="700">
+                    {tweet?.likes?.length}
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">
+                    Likes
+                  </Typography>
+                </Stack>
+              </>
+            )}
+
+            <Stack spacing={0.5}>
+              <Divider />
+              <Stack direction="row" justifyContent="space-around">
+                <ReplyButton size="medium" handleClick={openModal} />
+                <RetweetButton size="medium" />
+                <LikeButton
+                  tweetId={tweet?.id}
+                  likes={tweet?.likes}
+                  size="medium"
+                />
+                <ShareButton
+                  size="medium"
+                  tweetId={tweet.id}
+                  tweetUsername={tweet.user.username}
+                />
+              </Stack>
+              <Divider />
+            </Stack>
+
+            <TweetForm
+              tweet={tweet}
+              fileInputId={`reply-details-file-input-${tweet.id}`}
             />
           </Stack>
-          <Divider />
-        </Stack>
 
-        <TweetForm
-          tweet={tweet}
-          fileInputId={`reply-details-file-input-${tweet.id}`}
-        />
-      </Stack>
-      <TweetList id={tweet.id} />
-      <CustomModal open={open} handleClose={closeModal}>
-        <TweetForm
-          tweet={tweet}
-          fileInputId={`reply-details-modal-file-input-${tweet.id}`}
-          handleClose={closeModal}
-          showParent
-        />
-      </CustomModal>
+          <TweetList id={tweet.id} />
+          <CustomModal open={open} handleClose={closeModal}>
+            <TweetForm
+              tweet={tweet}
+              fileInputId={`reply-details-modal-file-input-${tweet.id}`}
+              handleClose={closeModal}
+              showParent
+            />
+          </CustomModal>
+        </>
+      )}
     </Stack>
   );
 };
