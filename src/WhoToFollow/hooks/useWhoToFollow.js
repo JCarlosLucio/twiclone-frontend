db4c4ee3f51b1e42d32e70ddd03f724c07355731
jcLucio <c.lucio3@gmail.com>
2021-10-29 22:55:04 -0500

    Add useWhoToFollow custom hook

---
+++ src/WhoToFollow/hooks/useWhoToFollow.js
@@ -0,0 +1,12 @@
+import { useQuery } from 'react-query';
+import { queryKeys } from '../../constants';
+import { getWhoToFollow } from '../../services/user';
+import { useMe } from '../../shared/hooks/useMe';
+
+export const useWhoToFollow = () => {
+  const { me } = useMe();
+  const { data: users, isLoading } = useQuery([queryKeys.whotofollow], () =>
+    getWhoToFollow(me.id)
+  );
+  return { users, isLoading };
+};
