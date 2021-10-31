1a6444522ce2589d320255d115065e37bf0154e9
jcLucio <c.lucio3@gmail.com>
2021-10-31 10:15:26 -0600

    Add Avatar/UserHeader to WhoToFollow List component

--- src/Info/WhoToFollow.jsx
+++ src/Info/WhoToFollow.jsx
@@ -1,4 +1,11 @@
-import { Loading } from '../shared';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemButton from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+import { Loading, UserHeader } from '../shared';
 import { useWhoToFollow } from './hooks/useWhoToFollow';
 
 export const WhoToFollow = () => {
@@ -7,10 +14,24 @@ export const WhoToFollow = () => {
   if (isLoading) return <Loading />;
 
   return (
-    <div>
+    <List
+      sx={{ maxWidth: 350, borderRadius: '1rem' }}
+      subheader={
+        <ListSubheader component="div" sx={{ px: 2, py: 0.5 }}>
+          <Typography variant="h6">Who to follow</Typography>
+        </ListSubheader>
+      }
+    >
       {users.map((user) => (
-        <p key={user.id}>{user.name}</p>
+        <ListItemButton key={user?.id}>
+          <ListItemAvatar>
+            <Avatar src={user?.avatar?.url} alt={`${user?.name}`} />
+          </ListItemAvatar>
+          <ListItemText>
+            <UserHeader user={user} direction="column" withLink />
+          </ListItemText>
+        </ListItemButton>
       ))}
-    </div>
+    </List>
   );
 };
