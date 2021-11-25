84014eaab636b35e77f00209126c6fca7e60ab35
jcLucio <c.lucio3@gmail.com>
2021-11-24 18:54:15 -0600

    Add navigation to / after successful register

--- src/Auth/hooks/useRegister.js
+++ src/Auth/hooks/useRegister.js
@@ -1,13 +1,16 @@
 import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router';
 import { register } from '../../services/auth';
 import { useMe } from '../../shared/hooks/useMe';
 
 export const useRegister = () => {
   const { updateUser } = useMe();
+  const navigate = useNavigate();
 
   const { mutate: registerUser, isLoading } = useMutation(register, {
     onSuccess: (data) => {
       updateUser(data);
+      navigate('/');
     },
   });
 
