bbbfe4fe517772be3723727bef7787f8fc2c90c2
jcLucio <c.lucio3@gmail.com>
2021-11-24 18:53:22 -0600

    Add navigation to / after successful login

--- src/Auth/hooks/useLogin.js
+++ src/Auth/hooks/useLogin.js
@@ -1,13 +1,16 @@
 import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router';
 import { login } from '../../services/auth';
 import { useMe } from '../../shared/hooks/useMe';
 
 export const useLogin = () => {
   const { updateUser } = useMe();
+  const navigate = useNavigate();
 
   const { mutate: loginUser, isLoading } = useMutation(login, {
     onSuccess: (data) => {
       updateUser(data);
+      navigate('/');
     },
   });
 
