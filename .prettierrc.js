48c7cedbbabd7cc4f908184625374dfbf25e56e7
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-06-19 16:08:20 -0600

    Update prettier rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,23 @@
+/** @type {import("prettier").Config} */
 module.exports = {
-  trailingComma: 'es5',
+  arrowParens: 'always',
+  quoteProps: 'as-needed',
   semi: true,
+  trailingComma: 'all',
   singleQuote: true,
+  printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   bracketSpacing: true,
-  printWidth: 80,
   proseWrap: 'always',
+  endOfLine: 'auto',
+  jsxSingleQuote: false,
+  embeddedLanguageFormatting: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+  singleAttributePerLine: false,
+  vueIndentScriptAndStyle: false,
+  requirePragma: false,
+  insertPragma: false,
 };
+
+// jsxBracketSameLine: false // [deprecated]
